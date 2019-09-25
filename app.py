e897ca3c73416f8329242b8ed4afb1a2e7752f9c
Jason <jasonnova@protonmail.com>
2019-09-25 07:06:04 -0700

    Added one more minor endpoint for all possible years

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ df = pd.read_csv('test.csv')
 
 # Flask App Factory
 application = Flask(__name__)
+# Wrap application in CORS so that it when calling from localhost
+# addresses the computer won't disallow the request...due to CORS.
 CORS(application)
 
 
@@ -23,21 +25,21 @@ def test():
 @application.route("/reception", methods=['GET'])
 def retrieval():
     '''
-    Here we should get the country and year they asked for.
+    Here we should get the data for the country and year they asked for.
     '''
     try:
         if request.method == 'GET':
             country = request.args.get('country')  # If no key then null
             year = request.args.get('year')  # If no key then null
             return spout(country, year)
-            # If they do a GET it auto doesn't allow tmyk wow
     except Exception as e:
+        # Unfortunately I'm not going to wrap this in indv. strings
         r = Response(response=f"""
 Something broke,
-perhaps the year (1990-2016) or country name is wrong.
+perhaps the year or country name is wrong.
 !!!{e}!!!
-Use the /countries endpoint to get a list
-of country names in case.
+Use the /countries or /years endpoints to get a list
+of possible values.
 """,
                      status=404,
                      mimetype="application/xml")
@@ -45,13 +47,27 @@ of country names in case.
         return r
 
 
-# This endpoint gives them a big list of possible country names in JSON
+# This endpoint gives a big list of possible country names in JSON
 @application.route("/countries", methods=['GET'])
 def cNames():
     '''
     Returns a JSON list of country names
     '''
     a = pd.DataFrame(df['Country Name'].unique(), columns=['cname']).to_json()
+    r = Response(response=a,
+                 status=200,
+                 mimetype="application/json")
+    r.headers["Content-Type"] = "text/json; charset=utf-8"
+    return r
+
+
+# This endpoint gives a list of possible years in JSON
+@application.route("/years", methods=['GET'])
+def aYears():
+    '''
+    Returns a JSON list of years
+    '''
+    a = pd.DataFrame(df['Year'].unique(), columns=['years']).to_json()
     r = Response(response=a,
                  status=200,
                  mimetype="application/json")
